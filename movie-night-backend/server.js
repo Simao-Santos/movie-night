0f3b7d69c5337b9a05334dcb8b0768cd882f6cfc
Simao-Santos <simao.p.m.santos@gmail.com>
2023-06-15 18:37:31 +0100

    yay fetch data from express

--- movie-night-backend/server.js
+++ movie-night-backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.json({ "users": ["bananas", "maça", "canela"] })
 })
 
--- movie-night-vite/src/App.jsx
+++ movie-night-vite/src/App.jsx
@@ -1,13 +1,41 @@
+import React, { useEffect, useState } from 'react'
 import Navbar from "./components/Navbar"
 import MovieOptions from "./pages/MovieOptions"
 import OptionsGenerator from "./pages/OptionsGenerator"
 
 function App() {
 
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
   return (
     <>
       <Navbar></Navbar>
       {/* <OptionsGenerator></OptionsGenerator> */}
+
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      {(typeof backendData.users === 'undefined') ? (
+        <p>loading...</p>
+      ) : (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+
+
       <MovieOptions></MovieOptions>
     </>
   )
